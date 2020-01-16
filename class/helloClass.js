3dc7a8e30bc87b16fffae8a79fadb746a12382d4
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-16 19:14:14 +0900

    modify : add mehod

--- class/helloClass.js
+++ class/helloClass.js
@@ -3,6 +3,10 @@ class MyClass {
     // thisはclass自身
     this.myName = name;
   }
+  getMyName() {
+    return this.myName;
+  }
 }
 const myInstance = new MyClass('太郎');
 console.log(myInstance.myName);
+console.log(myInstance.getMyName());
