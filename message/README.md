8a673757c41bcd50ec5fc5ec20979c590c8111a9
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-05-05 23:51:42 +0900

    途中 docker

---
+++ message/README.md
@@ -0,0 +1,9 @@
+# Message
+
+## Build
+
+docker-compose build
+
+## 起動
+
+docker-compose up -d
--- message/app/app.js
+++ message/app/app.js
@@ -1,4 +1,7 @@
 const WebSocketServer = require('ws').Server;
+const redis = require('redis');
+const redisSub = redis.createClient();
+const redisPub = redis.createClient();
 
 const server = require('http').createServer(
   require('ecstatic')({ root: `${__dirname}/www` })
@@ -9,14 +12,15 @@ wss.on('connection', (ws) => {
   console.log('Client connected!');
   ws.on('message', (msg) => {
     console.log(`Message: ${msg}`);
-    broadcast(msg);
+    redisPub.publish('chat_messages', msg);
   });
 });
 
-function broadcast(msg) {
+redisSub.subscribe('chat_messages');
+redisSub.on('message', (channel, msg) => {
   wss.clients.forEach((client) => {
     client.send(msg);
   });
-}
+});
 
 server.listen(process.argv[2] || 8080);
